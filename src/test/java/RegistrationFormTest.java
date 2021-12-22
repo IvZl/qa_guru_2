b82ee83d8bf9ae7d3a608c8ef5780b73c59c8cb2
IvZl <shepelev007@gmail.com>
2021-12-23 00:22:48 +0300

    Second try. Test doesn't work yet.

--- src/test/java/RegistrationFormTest.java
+++ src/test/java/RegistrationFormTest.java
@@ -2,8 +2,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.byId;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -21,12 +20,19 @@ public class RegistrationFormTest {
         $(byId("userEmail")).setValue("test@mail.ru");
         $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("1234567890");
-        $("[aria-label$='March 1st, 1992']").click(); //Don't work?!
-//        $("[aria-label='Choose Sunday, March 1st, 1992']").click(); // Also don't work?????!
+        $("#dateOfBirthInput").scrollTo().click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("November");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("1998");
+//        $("react-datepicker__day react-datepicker__day--003").click(); Don't work?!
+        $("[class*='react-datepicker__day--030']").click();
+//      $("[aria-label$='March 1st, 1992']").click(); //Don't work?!
+//      $("[aria-label='Choose Sunday, March 1st, 1992']").click(); // Also don't work?????!
         $("subjectsInput").setValue("Economics").pressEnter();
         $("hobbiesWrapper").$(byText("Sport")).click();
         $("currentAddress").setValue("TestAddress");
-//        $("uploadPicture").uploadFile(new File("src/test/resources/images/test.png"));
+//      $("uploadPicture").uploadFile(new File("src/test/resources/images/test.png"));
         $("uploadPicture").uploadFromClasspath("images/test.png");
 
         $("#state").click();
